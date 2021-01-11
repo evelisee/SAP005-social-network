b846ce1c360d44d2db707ce7ccb45dee8a09580e
eeveelise <evelise.dib@loft.com.br>
2021-01-11 15:12:18 -0300

    fix: refactor comment and like

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,9 +1,8 @@
-import { likedPost } from "../../services/index.js";
-import { onNavigate } from '../../utils/history.js'
+import { likedPost, commentPost } from "../../services/index.js";
   
 export const Home = () => {
+  console.log('caiu no home');
   // Coloque sua página
-  console.log('carregou de novo');
   const rootElement = document.createElement('div');
   rootElement.innerHTML = `
       <h1>Timeline</h1>
@@ -13,8 +12,9 @@ export const Home = () => {
   `;
 
 
+  //---- Mock para gerar vários posts na tela
+
   const timeline = rootElement.querySelector('.container-posts');
-  //Mock para gerar vários posts na tela
   const postsLoop = Array.from(Array(10).keys());
   postsLoop.forEach(() => {
     timeline.innerHTML += `
@@ -25,26 +25,83 @@ export const Home = () => {
         <button class="like">Curtir</button>
         <button class="comentar">Comentar</button>
         <button class="compartilhar">Compartilhar</button>
+
+        <div class="on-comment">
+          <hr>
+          <textarea class="comentar-text"></textarea>
+          <button class="enviar-comentario">enviar</button>
+        </div>
       </div>
     `
   });
 
+  //---- Fluxo de Like
+
   const likeButtonArray = rootElement.querySelectorAll('.like');
+
   likeButtonArray.forEach((button) => {
     button.addEventListener('click', (e) => {
       executeLike(e);
-    })
-  })
-
+    });
+  });
 
   const executeLike = (e) => {
-    likedPost().then(() => {
-      console.log('clicou');
-      console.log(e);
-      // onNavigate('/');
-    }).catch(() => {
+    likedPost()
+    .then((retorno) => {
+      console.log(retorno);
+      e.target.classList.add('liked');
+    })
+    .catch(() => {
       alert('Deu rum ai meu kerido');
     });
   }
+
+  //---- Fluxo de Comment
+
+  const comentButtonArray = rootElement.querySelectorAll('.comentar');
+  comentButtonArray.forEach((buttonComment) => {
+    buttonComment.addEventListener('click', (event) => {
+      showCommentBox(event);
+    });
+  });
+
+  const showCommentBox = (event) => {
+    const cardPost = event.target.parentNode;
+    toggleCommentBox(cardPost, true);
+
+    const buttonEnviarComment = cardPost.querySelector('.enviar-comentario');
+
+    buttonEnviarComment.addEventListener('click', () => {
+      executeComment(cardPost);
+    });
+  }
+
+  const executeComment = (cardPost) => {
+    const textareaComment = cardPost.querySelector('.comentar-text');
+    const holderCommentBlock = cardPost.querySelector('.on-comment');
+  
+    commentPost(textareaComment.value)
+      .then(() => {
+        const textPValue = document.createElement("p");
+        textPValue.textContent = textareaComment.value;
+        cardPost.insertBefore(textPValue, holderCommentBlock);
+        toggleCommentBox(cardPost, false);
+        textareaComment.value = "";
+      })
+      .catch(() => {
+        alert('Deu ruim aí');
+      })
+  }
+
+  const toggleCommentBox = (cardPost, show) => {
+    const holderCommentBlock = cardPost.querySelector('.on-comment');
+
+    if(show) {
+      holderCommentBlock.classList.add('display');
+    } else {
+      holderCommentBlock.classList.remove('display');
+    }
+  }
+
   return rootElement;
 };
--- src/services/index.js
+++ src/services/index.js
@@ -1,5 +1,6 @@
 const firestore = firebase.firestore();
 const likesCollection = firestore.collection('likes');
+
 export const myFunction = () => {
   // seu código aqui
   console.log('Olá mundo!');
@@ -10,6 +11,23 @@ export const likedPost = () => {
   return likesCollection.add({
     liked: true,
   })
-  .then(() => Promise.resolve(true))
-  .catch((error) => Promise.reject(error))
+  .then(() => {
+    return Promise.resolve(true);
+  })
+  .catch((error) => { 
+    return Promise.reject(error);
+  })
+}
+
+export const commentPost = (comment) => {
+  console.log(comment);
+  return likesCollection.add({
+    liked: true,
+  })
+  .then(() => {
+    return true
+  })
+  .catch((error) => { 
+    return error;
+  })
 }
